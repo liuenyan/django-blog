8bbadb7f2bd437035541e3416f446e2f8db5487b
Enyan Liu <liu.enyan@gmail.com>
2016-05-13 15:38:12 +0800

    调整美化文章评论区。

--- blog/tools.py
+++ blog/tools.py
@@ -1,6 +1,5 @@
 from .models import Post, Tag, Comment
 from django.contrib.auth.models import User
-from functools import reduce
 from faker import Factory
 
 
@@ -9,10 +8,10 @@ def generate_fake_posts(nums=50):
     for i in range(nums):
         post = Post(title = fake.word(), body=fake.paragraph(), author=User.objects.get(pk=2))
         post.save()
-        generate_fake_comment(post)
+        generate_fake_comments(post)
 
 
-def generate_fake_comment(post, nums=10):
+def generate_fake_comments(post, nums=10):
     fake = Factory.create('en_US')
     for i in range(10):
         comment = Comment(name=fake.name(), email=fake.email(), url=fake.url(), comment=fake.paragraph(), post=post)
