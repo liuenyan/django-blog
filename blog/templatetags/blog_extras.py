bfc1bf2abb7c51851c7f7503f31609042eecb3df
Enyan Liu <liu.enyan@gmail.com>
2016-05-18 16:46:10 +0800

    gravatar评论头像支持。

--- blog/templatetags/blog_extras.py
+++ blog/templatetags/blog_extras.py
@@ -1,6 +1,7 @@
 from django import template
 from ..models import Post, Tag, Link
 from django.db.models import Count
+import hashlib
 
 register = template.Library()
 
@@ -10,3 +11,9 @@ def show_sidebar():
     archives = Post.objects.extra(select={'year': 'strftime("%Y", timestamp)', 'month': 'strftime("%m", timestamp)'}).values('year', 'month').annotate(Count('id'))
     links = Link.objects.all()
     return {'tags': tags, 'archives': archives, 'links': links}
+
+@register.filter
+def gravatar(email, size=60, default='identicon'):
+    md5 = hashlib.md5(email.encode('utf8').lower()).hexdigest()
+    gravatar_url = "//cdn.v2ex.com/gravatar/{0}?s={1}&d={2}".format(md5, str(size), default)
+    return gravatar_url
