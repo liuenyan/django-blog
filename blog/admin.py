8346aba9065d78d1c28dfe61ea9d052c38cd2b23
Enyan Liu <liu.enyan@gmail.com>
2016-07-11 14:10:42 +0800

    在管理界面将body_html设为只读。

--- blog/admin.py
+++ blog/admin.py
@@ -3,7 +3,11 @@ from .models import Post, Comment, Tag, Link
 
 # Register your models here.
 
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    readonly_fields = ('body_html',)
+
+
+admin.site.register(Post, PostAdmin)
 admin.site.register(Comment)
 admin.site.register(Tag)
 admin.site.register(Link)
