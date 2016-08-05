0b7df93c08f8c4169dc8ac7271eb15af29ffab26
Enyan Liu <liu.enyan@gmail.com>
2016-08-05 12:24:49 +0800

    使用less重写样式表 / 重命名压缩后的文件

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,29 @@
 var gulp = require('gulp'),
     uglify = require('gulp-uglify'),
-    minifyCSS = require('gulp-minify-css');
+    minifyCSS = require('gulp-minify-css'),
+    less = require('gulp-less'),
+    rename = require('gulp-rename');
 
-gulp.task('default', ['script', 'css']);
+gulp.task('default', ['script', 'less']);
 
 gulp.task('script', function(){
   gulp.src('blog/static/blog/js/*.js')
       .pipe(uglify())
+      .pipe(rename({suffix:'.min'}))
       .pipe(gulp.dest('blog/static/blog/dist/js/'));
 });
 
 gulp.task('css', function(){
-  gulp.src('blog/static/blog/styles/*.css')
+  gulp.src('blog/static/blog/css/*.css')
       .pipe(minifyCSS())
+      .pipe(rename({suffix:'.min'}))
+      .pipe(gulp.dest('blog/static/blog/dist/css/'));
+});
+
+gulp.task('less', function(){
+  gulp.src('blog/static/blog/css/*.less')
+      .pipe(less())
+      .pipe(minifyCSS())
+      .pipe(rename({suffix:'.min'}))
       .pipe(gulp.dest('blog/static/blog/dist/css/'));
 });
