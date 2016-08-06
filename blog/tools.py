3fd07dc11d1bc89a99e8e75b650da92e89d37556
Enyan Liu <liu.enyan@gmail.com>
2016-08-06 17:26:29 +0800

    删除生成文章数据的辅助函数。

--- blog/tools.py
+++ blog/tools.py
@@ -3,33 +3,8 @@
 """
 import bleach
 import markdown
-from django.contrib.auth.models import User
-from faker import Factory
 from bleach_whitelist import generally_xss_safe
 from blog.models import Post, Comment
-
-def generate_fake_posts(nums=50):
-    fake = Factory.create('en_US')
-    for _ in range(nums):
-        post = Post(
-            title=fake.word(),
-            body=fake.paragraph(),
-            author=User.objects.get(pk=2)
-        )
-        post.save()
-        generate_fake_comments(post)
-
-def generate_fake_comments(post, nums=10):
-    fake = Factory.create('en_US')
-    for _ in range(nums):
-        comment = Comment(
-            name=fake.name(),
-            email=fake.email(),
-            url=fake.url(),
-            comment=fake.paragraph(),
-            post=post
-        )
-        comment.save()
 
 def clean_html_tags(data):
     return bleach.linkify(
